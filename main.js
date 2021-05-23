3ba2fa89cbda4acdd3c20b3425f31aa38ef50dc5
merlynnvp <72741790+merlynnvp@users.noreply.github.com>
2021-05-23 20:53:13 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+noseX=0;
+noseY=0;
+diffrence = 0;
+rightWristX = 0;
+leftWristX = 0;
 function setup()
 {
     video = createCapture(VIDEO);
@@ -15,15 +20,30 @@ function modelLoaded()
     console.log('PoseNet Is Initialized!');
 }
 
-function draw()
-{
-    background('#969A97');
-}
-
 function gotPoses(results)
 {
     if(results.length > 0)
     {
         console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("noseX = " + noseX + "noseX = " + noseY);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        diffrence = leftWristX - rightWristX;
+
+        console.log("leftWristX =" + leftWristX +"rightWristX = "+ rightWristX + " diffrence = " + diffrence);
     }
 }
+
+
+function draw()
+{
+    background('#969A97');
+    fill('#F90093');
+    stroke('#F90093');
+    text("merlyn", noseX, noseY);
+    textSize(diffrence);
+}
+
